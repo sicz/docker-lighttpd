12043ecf1a0e647f0b08897f760c219050ac9bf1
Petr Řehoř <rx@rx.cz>
2017-08-27 23:38:30 +0200

    Remove unnecessary code

--- config/docker-entrypoint.d/47-lighttpd-cert.sh
+++ config/docker-entrypoint.d/47-lighttpd-cert.sh
@@ -7,7 +7,6 @@
 if [ -e ${SERVER_CRT_FILE} -a -e ${SERVER_KEY_FILE} ]; then
   info "Creating lighttpd certificate file ${SERVER_CRT_FILE}"
   openssl rsa -in ${SERVER_KEY_FILE} -passin pass:${SERVER_KEY_PWD} >> ${SERVER_CRT_FILE}
-  chown lighttpd:lighttpd ${SERVER_CRT_FILE}
   rm -f ${SERVER_KEY_FILE}
 elif [ -e ${SERVER_CRT_FILE} ]; then
   info "Using lighttpd certificate file ${SERVER_CRT_FILE}"
